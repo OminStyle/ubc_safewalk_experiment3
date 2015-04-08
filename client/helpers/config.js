74fc60f983d73871620913aaf40f7fca2e9a1b8d
Zoe <nahj222@gmail.com>
2015-04-08 13:42:21 -0700

    Update config.js
    Changed the wait time

--- client/helpers/config.js
+++ client/helpers/config.js
@@ -4,9 +4,9 @@ Accounts.ui.config({
 
 if (Meteor.isClient) {
   Meteor.startup(function () {
-    Session.set('shortWait', 22);
-    Session.set('longWait', 30);
+    Session.set('shortWait', 12);
+    Session.set('longWait', 22);
     Session.set('loseRate', 0.65); // 1 = 100%, 0.5 = 50%meteor
-    Session.set('gameLength', 60);
+    Session.set('gameLength', 999); 
   });
 }
